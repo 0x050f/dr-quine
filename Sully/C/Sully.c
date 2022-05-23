7e7b4f51cdb4202d8d5a0d00137e80c3df703890
Yellow <lmartin@student.42.fr>
2022-05-23 23:23:45 +0200

    didn't understand the Sully subject but got it now :))

--- Sully/C/Sully.c
+++ Sully/C/Sully.c
@@ -1,27 +1,31 @@
 #include <fcntl.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/stat.h>
 
-#define FILENAME "Sully_%d.c"
+char *code = "#include <fcntl.h>%1$c#include <stdlib.h>%1$c#include <stdio.h>%1$c#include <unistd.h>%1$c#include <sys/stat.h>%1$c%1$cchar *code = %3$c%6$s%3$c;%1$c%1$cint		main(void)%1$c{%1$c	char filename[256];%1$c	char command[256];%1$c	int i = %4$d;%1$c	int j = %5$d;%1$c%1$c	if (i <= 0)%1$c		return (0);%1$c	else if (!j)%1$c		sprintf(filename, %3$cSully_%2$cd.c%3$c, --i);%1$c	else%1$c		sprintf(filename, %3$cSully_%2$cd.c%3$c, i);%1$c	int fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0644);%1$c	chmod(filename, 0644);%1$c	if (fd < 0)%1$c		return (1);%1$c	dprintf(fd, code, 10, 37, 34, i, !i, code);%1$c	close(fd);%1$c	sprintf(command, %3$cgcc -Wall -Wextra -Werror Sully_%2$c1$d.c -o Sully_%2$c1$d; ./Sully_%2$c1$d%3$c, i);%1$c	system(command);%1$c	return (0);%1$c}%1$c";
 
 int		main(void)
 {
-	char *code = "#include <fcntl.h>%1$c#include <stdio.h>%1$c#include <unistd.h>%1$c#include <sys/stat.h>%1$c%1$c#define FILENAME %3$cSully_%2$cd.c%3$c%1$c%1$cint		main(void)%1$c{%1$c	char *code = %3$c%5$s%3$c;%1$c	int i = %4$d;%1$c%1$c	if (i <= 0)%1$c		return (0);%1$c	for (; i >= 0; i--)%1$c	{%1$c		char filename[256];%1$c		sprintf(filename, FILENAME, i);%1$c		chmod(filename, 0644);%1$c		int fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0644);%1$c		if (fd < 0)%1$c			continue ;%1$c		dprintf(fd, code, 10, 37, 34, i, code);%1$c		close(fd);%1$c	}%1$c	return (0);%1$c}%1$c";
+	char filename[256];
+	char command[256];
 	int i = 5;
+	int j = 1;
 
 	if (i <= 0)
 		return (0);
-	for (; i >= 0; i--)
-	{
-		char filename[256];
-		sprintf(filename, FILENAME, i);
-		chmod(filename, 0644);
-		int fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0644);
-		if (fd < 0)
-			continue ;
-		dprintf(fd, code, 10, 37, 34, i, code);
-		close(fd);
-	}
+	else if (!j)
+		sprintf(filename, "Sully_%d.c", --i);
+	else
+		sprintf(filename, "Sully_%d.c", i);
+	int fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0644);
+	chmod(filename, 0644);
+	if (fd < 0)
+		return (1);
+	dprintf(fd, code, 10, 37, 34, i, !i, code);
+	close(fd);
+	sprintf(command, "gcc -Wall -Wextra -Werror Sully_%1$d.c -o Sully_%1$d; ./Sully_%1$d", i);
+	system(command);
 	return (0);
 }
