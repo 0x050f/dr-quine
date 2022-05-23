392bb61c3c09e432a0d8ebf6a0c5ba5c06620a8e
Yellow <y3ll0w42.dev@gmail.com>
2022-05-23 05:22:27 -0400

    update asm Grace

--- Grace/ASM/Grace.s
+++ Grace/ASM/Grace.s
@@ -2,20 +2,20 @@ section.text:
 	global _start
 	extern dprintf
 
-%define FILENAME		`Grace_kid.s`
-%define FILE_RIGHTS		420
-%define FLAGS			577
+%define		FILENAME		`Grace_kid.s`
+%define ㅤ	_start
+%define		beep_boop boop_beep
 
 ; This is a _start. No shit ?
-_start:
+ㅤ:
 	lea rdi, [rel filename]
-	mov rsi, FILE_RIGHTS
+	mov rsi, 420
 	mov rax, 90
 	syscall
 
 	push rsi
 	pop rdx
-	mov rsi, FLAGS
+	mov rsi, 577
 	mov rax, 2
 	syscall
 
@@ -35,5 +35,5 @@ _start:
 	mov rax, 60
 	syscall
 
-code db `section.text:%1$c	global _start%1$c	extern dprintf%1$c%1$c%2$cdefine FILENAME		%3$cGrace_kid.s%3$c%1$c%2$cdefine FILE_RIGHTS		420%1$c%2$cdefine FLAGS			577%1$c%1$c; This is a _start. No shit ?%1$c_start:%1$c	lea rdi, [rel filename]%1$c	mov rsi, FILE_RIGHTS%1$c	mov rax, 90%1$c	syscall%1$c%1$c	push rsi%1$c	pop rdx%1$c	mov rsi, FLAGS%1$c	mov rax, 2%1$c	syscall%1$c%1$c	push rax%1$c	pop rdi%1$c	lea rsi, [rel code]%1$c	mov rdx, 10%1$c	mov rcx, 37%1$c	mov r8, 96%1$c	mov r9, rsi%1$c	call dprintf%1$c%1$c	mov rax, 3%1$c	syscall%1$c%1$c	xor rdi, rdi%1$c	mov rax, 60%1$c	syscall%1$c%1$ccode db %3$c%4$s%3$c, 0x0%1$cfilename db FILENAME, 0x0%1$c`, 0x0
+code db `section.text:%1$c	global _start%1$c	extern dprintf%1$c%1$c%2$cdefine		FILENAME		%3$cGrace_kid.s%3$c%1$c%2$cdefine ㅤ	_start%1$c%2$cdefine		beep_boop boop_beep%1$c%1$c; This is a _start. No shit ?%1$cㅤ:%1$c	lea rdi, [rel filename]%1$c	mov rsi, 420%1$c	mov rax, 90%1$c	syscall%1$c%1$c	push rsi%1$c	pop rdx%1$c	mov rsi, 577%1$c	mov rax, 2%1$c	syscall%1$c%1$c	push rax%1$c	pop rdi%1$c	lea rsi, [rel code]%1$c	mov rdx, 10%1$c	mov rcx, 37%1$c	mov r8, 96%1$c	mov r9, rsi%1$c	call dprintf%1$c%1$c	mov rax, 3%1$c	syscall%1$c%1$c	xor rdi, rdi%1$c	mov rax, 60%1$c	syscall%1$c%1$ccode db %3$c%4$s%3$c, 0x0%1$cfilename db FILENAME, 0x0%1$c`, 0x0
 filename db FILENAME, 0x0
