b21d1411e8a3c6607ea4eef3e0702181ed126a8d
y3ll0w42 <y3ll0w42.dev@gmail.com>
2022-05-24 10:34:23 +0200

    weird behaviour not vm

--- Grace/ASM/Grace.s
+++ Grace/ASM/Grace.s
@@ -3,8 +3,8 @@ section.text:
 	extern dprintf
 
 %define		FILENAME		`Grace_kid.s`
-%define ㅤ	_start:
-%macro		⠀ 0
+%define FT	_start:
+%macro		xxx 0
 	lea rdi, [rel filename]
 	mov rsi, 420
 	mov rax, 90
@@ -32,12 +32,12 @@ section.text:
 	mov rax, 60
 	syscall
 
-code db `section.text:%1$c	global _start%1$c	extern dprintf%1$c%1$c%2$cdefine		FILENAME		%3$cGrace_kid.s%3$c%1$c%2$cdefine ㅤ	_start:%1$c%2$cmacro		⠀ 0%1$c	lea rdi, [rel filename]%1$c	mov rsi, 420%1$c	mov rax, 90%1$c	syscall%1$c%1$c	push rsi%1$c	pop rdx%1$c	mov rsi, 577%1$c	mov rax, 2%1$c	syscall%1$c%1$c	push rax%1$c	pop rdi%1$c	lea rsi, [rel code]%1$c	mov rdx, 10%1$c	mov rcx, 37%1$c	mov r8, 96%1$c	mov r9, rsi%1$c	call dprintf%1$c%1$c	mov rax, 3%1$c	syscall%1$c%1$c	xor rdi, rdi%1$c	mov rax, 60%1$c	syscall%1$c%1$ccode db %3$c%4$s%3$c, 0x0%1$cfilename db FILENAME, 0x0%1$c%2$cendmacro%1$c%1$c;%1$c; This is a _start. No shit ?%1$c;%1$cㅤ%1$c	⠀%1$c`, 0x0
+code db `section.text:%1$c	global _start%1$c	extern dprintf%1$c%1$c%2$cdefine		FILENAME		%3$cGrace_kid.s%3$c%1$c%2$cdefine FT	_start:%1$c%2$cmacro		xxx 0%1$c	lea rdi, [rel filename]%1$c	mov rsi, 420%1$c	mov rax, 90%1$c	syscall%1$c%1$c	push rsi%1$c	pop rdx%1$c	mov rsi, 577%1$c	mov rax, 2%1$c	syscall%1$c%1$c	push rax%1$c	pop rdi%1$c	lea rsi, [rel code]%1$c	mov rdx, 10%1$c	mov rcx, 37%1$c	mov r8, 96%1$c	mov r9, rsi%1$c	call dprintf%1$c%1$c	mov rax, 3%1$c	syscall%1$c%1$c	xor rdi, rdi%1$c	mov rax, 60%1$c	syscall%1$c%1$ccode db %3$c%4$s%3$c, 0x0%1$cfilename db FILENAME, 0x0%1$c%2$cendmacro%1$c%1$c;%1$c; This is a _start. No shit ?%1$c;%1$cFT%1$c	xxx%1$c`, 0x0
 filename db FILENAME, 0x0
 %endmacro
 
 ;
 ; This is a _start. No shit ?
 ;
-ㅤ
-	⠀
+FT
+	xxx
