8a59f06ded24e09c17043014237bf5b72f8879db
Yellow <y3ll0w42.dev@gmail.com>
2022-05-24 03:18:46 -0400

    chmod before open

--- Sully/C/Sully.c
+++ Sully/C/Sully.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <sys/stat.h>
 
-char *code = "#include <fcntl.h>%1$c#include <stdlib.h>%1$c#include <stdio.h>%1$c#include <unistd.h>%1$c#include <sys/stat.h>%1$c%1$cchar *code = %3$c%6$s%3$c;%1$c%1$cint		main(void)%1$c{%1$c	char filename[256];%1$c	char command[256];%1$c	int i = %4$d;%1$c	int j = %5$d;%1$c%1$c	if (i <= 0)%1$c		return (0);%1$c	else if (!j)%1$c		i--;%1$c	sprintf(filename, %3$cSully_%2$cd.c%3$c, i);%1$c	int fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0644);%1$c	chmod(filename, 0644);%1$c	if (fd < 0)%1$c		return (1);%1$c	dprintf(fd, code, 10, 37, 34, i, !i, code);%1$c	close(fd);%1$c	sprintf(command, %3$cgcc -Wall -Wextra -Werror Sully_%2$c1$d.c -o Sully_%2$c1$d; ./Sully_%2$c1$d%3$c, i);%1$c	system(command);%1$c	return (0);%1$c}%1$c";
+char *code = "#include <fcntl.h>%1$c#include <stdlib.h>%1$c#include <stdio.h>%1$c#include <unistd.h>%1$c#include <sys/stat.h>%1$c%1$cchar *code = %3$c%6$s%3$c;%1$c%1$cint		main(void)%1$c{%1$c	char filename[256];%1$c	char command[256];%1$c	int i = %4$d;%1$c	int j = %5$d;%1$c%1$c	if (i <= 0)%1$c		return (0);%1$c	else if (!j)%1$c		i--;%1$c	sprintf(filename, %3$cSully_%2$cd.c%3$c, i);%1$c	chmod(filename, 0644);%1$c	int fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0644);%1$c	if (fd < 0)%1$c		return (1);%1$c	dprintf(fd, code, 10, 37, 34, i, !i, code);%1$c	close(fd);%1$c	sprintf(command, %3$cgcc -Wall -Wextra -Werror Sully_%2$c1$d.c -o Sully_%2$c1$d; ./Sully_%2$c1$d%3$c, i);%1$c	system(command);%1$c	return (0);%1$c}%1$c";
 
 int		main(void)
 {
@@ -18,8 +18,8 @@ int		main(void)
 	else if (!j)
 		i--;
 	sprintf(filename, "Sully_%d.c", i);
-	int fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0644);
 	chmod(filename, 0644);
+	int fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0644);
 	if (fd < 0)
 		return (1);
 	dprintf(fd, code, 10, 37, 34, i, !i, code);
